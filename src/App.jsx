4885098c83b01d821a574852088221b6476ec951
robleto_dev <robletoae@outllok.com>
2023-05-24 08:42:28 -0600

    plantilla de trabajo para inicar proyectos con react + vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import '../src/styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+          Start the journey
+      </p>
+    </>
+  )
+}
+
+export default App
